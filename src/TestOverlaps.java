c602021151b769d900354bd1b614fa2fb5117004
AntonJohansson02 <76776791+AntonJohansson02@users.noreply.github.com>
2024-01-24 12:06:23 +0100

    klar

--- src/TestOverlaps.java
+++ src/TestOverlaps.java
@@ -1,25 +1,24 @@
 public class TestOverlaps {
     public static void main(String[] args){
-        // TODO: Step 1b: Note the static and dynamic types of these variables
-        Square square       = new Square(1,1);
-        Rectangle rectangle = new Rectangle(1,2);
-        Triangle triangle   = new Triangle(1,3);
+        Square square       = new Square(1,1); //static Square, dynamic Square
+        Rectangle rectangle = new Rectangle(1,2); //static Rectangle, dynamic Rectangle
+        Triangle triangle   = new Triangle(1,3); //static Triangle, dynamic Triangle
 
         // TODO: Step 1c: Predict what the result of these method calls will be.
         //   Fill in the blanks in the comment for each line before running the program.
-        square.overlaps(triangle);      // ___.overlaps:___{} vs ___{}
-        square.overlaps(square);        // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(rectangle);   // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(triangle);    // ___.overlaps:___{} vs ___{}
+        // square.overlaps(triangle);      // Polygon.overlaps: Polygon{} vs Triangle{}
+        // square.overlaps(square);        // Polygon.overlaps: Polygon{} vs Polygon{}
+        // triangle.overlaps(rectangle);   // Polygon.overlaps: Triangle{} vs Rectangle{}
+        // triangle.overlaps(triangle);    // Polygon.overlaps: Triangle{} vs Triangle{}
 
         // TODO: Step 2b: After you have uncommented all three overridden implementations
         //   of the method 'overlaps' in the subclasses, as per step 2a,
         //   again predict the result of the same method calls above.
         //   Fill in the blanks below before running the program.
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
+        // Square.overlaps: Square{} vs Triangle{}
+        // Square.overlaps: Square{} vs Square{}
+        // Triangle.overlaps: Triangle{} vs Rectangle{}
+        // Triangle.overlaps: Triangle{} vs Triangle{}
 
         // TODO: Step 3: Now we introduce a variable with static type Polygon. What will
         //   the results be this time? Fill in the blanks as before, then uncomment all
@@ -29,26 +28,26 @@ public class TestOverlaps {
         //   You can comment out the lines from above (10-13) using the same command.
         //   Then run the program again, and see if your predictions were correct.
         //   Can you explain these outputs in terms of static and dynamic types?
-        /*
+        
         Polygon polyTriangle = new Triangle(1,4);
 
-        square.overlaps(polyTriangle);          // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(polyTriangle);        // ___.overlaps:___{} vs ___{}
-        polyTriangle.overlaps(triangle);        // ___.overlaps:___{} vs ___{}
-        polyTriangle.overlaps(polyTriangle);    // ___.overlaps:___{} vs ___{}
-        triangle.overlaps(triangle);            // ___.overlaps:___{} vs ___{}
-        */
+        square.overlaps(polyTriangle);          // Square.overlaps: Square{} vs Triangle{}
+        triangle.overlaps(polyTriangle);        // Triangle.overlaps: Triangle{} vs Triangle{}
+        polyTriangle.overlaps(triangle);        // Triangle.overlaps: Triangle{} vs Triangle{}
+        polyTriangle.overlaps(polyTriangle);    // Triangle.overlaps: Triangle{} vs Triangle{}
+        triangle.overlaps(triangle);            // Triangle.overlaps: Triangle{} vs Triangle{}
 
         // TODO: Step 4b: After you have uncommented the overloaded declaration and
         //   implementation of the method 'overlaps' in the Triangle subclass,
         //   as per step 4a, again predict the output of the five lines above.
         //   Then run the program and see if your predictions were correct.
         //   Can you still explain the outputs in terms of static and dynamic types?
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
-        // ___.overlaps:___{} vs ___{}
+        //   Square.overlaps: Square{} vs Triangle{}
+        // Triangle.overlaps: Triangle{} vs Triangle{}
+        // Triangle.overlaps: Triangle{} vs Triangle{}
+        // Triangle.overlaps: Triangle{} vs Triangle{}
+        // Triangle2.overlaps: Triangle{} vs Triangle{}
+
 
     }//main
 
--- src/Triangle.java
+++ src/Triangle.java
@@ -14,19 +14,19 @@ public class Triangle extends Polygon {
     }
 
     // TODO: Step 2a: Uncomment this overridden method declaration.
-    /*
+    
      @Override
      public void overlaps(Polygon other) {
          overlapsHelper("Triangle", this.toString(), other.toString());
      }
-    */
+    
 
     // TODO: Step 4a: Uncomment this overloaded method declaration.
-    /*
+    
      public void overlaps(Triangle other) {
          overlapsHelper("Triangle2", this.toString(), other.toString());
      }
-    */
+    
 
     @Override
     public void paint(Graphics g){
